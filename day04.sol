cd41727cf0115e5007c897feba92f62f4aefabce
singhosanna <84977473+singhosanna@users.noreply.github.com>
2025-11-07 19:55:15 +0800

    Refactor auctionhouse contract for clarity and functionality

--- day04.sol
+++ day04.sol
@@ -2,35 +2,38 @@
 pragma solidity ^0.8.0;
 contract auctionhouse{
   address owner;
-  uint highestbid;
-  address highestbidder;
   uint endtime;
-  bool isended;
+  bool isend;
+  uint public highestbid;
+  address public highestbidder;
   string item;
-  address[] public bidders;
-  mapping(address=>uint) public bids;
+  address[] public bidder;
+  mapping(address=>uint) public bidamount;
   constructor(string memory _item,uint _biddingtime){
-    owner=msg.sender;
-    item=_item;
-    endtime=block.timestamp+_biddingtime;
+  owner=msg.sender;
+  endtime=block.timestamp+_biddingtime;
+  item=_item;
+  isend=false;
+}
+  function bid(uint amount)external{
+  require(block.timestamp < endtime, "Auction is ended.");
+  require(!isend,"auction is ended");
+  require(amount>0,"bid amount is not supported");
+  require(msg.sender!=owner,"you are owner");
+  require(amount>bidamount[msg.sender],"bid amount must be larger than the previous bid");
+  if(bidamount[msg.sender]==0){
+    bidder.push(msg.sender);
   }
-  function bid(uint amount) external{
-    require(block.timestamp<endtime,"auction has ended");
-    require(amount>highestbid,"you need to bid higher");
-    require(msg.sender!=owner,"owner cannot bid");
-    require(msg.sender!=highestbidder,"you are already the highest bidder");
-    if(bids[msg.sender]==0){
-      bidders.push(msg.sender);
-    }
-    bids[msg.sender]=amount;
-    if(amount>highestbid){
-      highestbid=amount;
-      highestbidder=msg.sender;
-    }
-  }
-  function endauction()external{
-    require(block.timestamp>endtime,"auction hasn't ended");
-    require(!isended,"auction is ended");
-    isended=true;
+  bidamount[msg.sender]=amount;
+  if(bidamount[msg.sender]>highestbid){
+    highestbidder=msg.sender;
+    highestbid=amount;
   }
   }
+  function endauction() public{
+  require(block.timestamp < endtime, "Auction was ended");
+  require(!isend,"auction was already been ended");
+  require(msg.sender==owner,"you don't have the permission to end this auction");
+  isend=true;
+  }
+}
