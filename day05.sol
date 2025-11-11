362f16e9f9b1a50536b5f8a06283d3e74e561827
singhosanna <84977473+singhosanna@users.noreply.github.com>
2025-11-11 18:06:04 +0800

    Refactor adminOnly contract for variable names and functions

--- day05.sol
+++ day05.sol
@@ -2,45 +2,45 @@
 pragma solidity ^0.8.0;
 contract adminOnly{
   address public owner;
-  uint public treasureAmount;
-  mapping(address=>uint) public withdrawlAllowance;
-  mapping(address=>bool) public hasWithdrawed;
-  constructor(uint _treasureAmount){
+  uint public treasuryAmount;
+  mapping(address=>uint) public withdrawlAllwoance;
+  mapping(address=>bool) public hasWithdrawn;
+  constructor(uint _treasuryAmount){
     owner=msg.sender;
-    treasureAmount=_treasureAmount;
+    treasuryAmount=_treasuryAmount;
   }
   modifier onlyOwner{
     require(msg.sender==owner,"Only owner can call this function");
     _;
   }
-  function addTreasure(uint amount) public onlyOwner{
-  treasureAmount += amount;
+  function addTreasure(uint _addAmount) public onlyOwner{
+    treasuryAmount+=_addAmount;
   }
-  function approveWithdrawl(address user,uint amount) public onlyOwner{
-  require(amount<=treasureAmount,"exceeded the maximum treasury funds avaliable");
-  withdrawlAllowance[user]=amount;
+  function approveWithdrawl(address _user,uint _amount) public onlyOwner{
+    require(_amount<=treasuryAmount,"exceeded maximum treasuryAmount");
+    withdrawlAllwoance[_user]=_amount;
+    hasWithdrawn[_user]=false;
   }
-  function withdrawlTreasure(uint amount) public{
-    uint allowance=withdrawlAllowance[msg.sender];
+  function withdrawTreasure(uint _amount) public{
     if(msg.sender==owner){
-      require(amount<=treasureAmount,"exceeded the maximum funds avaliable");
-      treasureAmount -= amount;
-    }
+      require(_amount<=treasuryAmount,"exceeded maximum treasuryAmount");
+      treasuryAmount-=_amount;
+  }
     else{
-      require(allowance>0, "You don't have any treasure allowance");
-      require(allowance>=amount,"exceeded the maximum funds avaliable");
-      require(allowance<=treasureAmount,"exceeded the maximum treasury funds avaliable");
-      require(!hasWithdrawed[msg.sender],"You have already withdrawed");
-      treasureAmount-=amount;
-      withdrawlAllowance[msg.sender]=0;
-      hasWithdrawed[msg.sender]=true;
+      require(withdrawlAllwoance[msg.sender]>=_amount,"insufficient allowance");
+      require(withdrawlAllwoance[msg.sender]>0,"insufficient allowance");
+      require(!hasWithdrawn[msg.sender],"you have already withdrawed");
+      require(_amount<=treasuryAmount,"exceeded maximum treasuryAmount");
+      withdrawlAllwoance[msg.sender]-=_amount;
+      treasuryAmount-=_amount;
+      hasWithdrawn[msg.sender]=true;
     }
   }
-  function resetWithdrawlStatus(address _user) onlyOwner public {
-    hasWithdrawed[_user]=false;
+  function resetWithdrawlStatus(address _user) public onlyOwner{
+    hasWithdrawn[_user]=false;
   }
-  function transferOwnership(address _owner) onlyOwner public {
-  require(_owner!=address(0),"Invalid address");
-  owner=_owner;
+  function transferOwnership(address _owner) public onlyOwner{
+    require(_owner!=address(0),"invalid address");
+    owner=_owner;
   }
 }
